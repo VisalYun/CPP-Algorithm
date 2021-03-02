6f85da8fa89ff54dd006f82260dc29cfd2afbf57
VisalYun <yunvisal18@kit.edu.kh>
2021-03-02 20:03:33 +0700

    add ABC 042

---
+++ Atcoder/ABC-042/C.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool checkIndex(vector<int> D, int K){
+	
+}
+
+int main(){
+	int N, K;	
+	cin >> N >> K;
+	vector<int> D(K);
+	for(int i=0; i<K; i++) cin>>D.at(i); 
+	
+	
+	return 0;
+}
